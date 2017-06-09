0355c3ac068f8117e418aaced52dd86585398b42
Tom Young <tyoung@n.io>
2017-06-09 12:04:09 -0600

    Create failing unit test

--- neural_network_block.py
+++ neural_network_block.py
@@ -5,12 +5,13 @@ from enum import Enum
 import tensorflow as tf
 
 
+# not used just yet, hardocoding mnist project values for now...
 # class ActivationFunctions(Enum):
     # softmax = 'softmax'
 
 # class Layers(PropertyHolder):
 
-    # count = IntProperty(default=1, title='Number of Neurons')
+    # count = IntProperty(title='Number of Neurons', default=10)
     # activation = SelectProperty(ActivationFunctions,
                                 # title='Activation Function',
                                 # default=ActivationFunctions.softmax)
@@ -25,6 +26,7 @@ class NeuralNetwork(Block):
     # set random seed for repeatable computations
     tf.set_random_seed(0)
     # input images [minibatch size, height, width, color channels]
+    # todo: verify order of height/width args
     X = tf.placeholder(tf.float32, [None, 28, 28, 1])
     # desired output
     Y_ = tf.placeholder(tf.float32, [None, 10])
@@ -35,17 +37,36 @@ class NeuralNetwork(Block):
     # flatten images
     XX = tf.reshape(X, [-1, 784])
 
+    def __init__(self):
+        self.loss_function = None
+        self.train_Step = None
+        self.sess = None
+        self.correct_prediction = None
+        self.accuracy = None
+        super().__init__()
+
     def start(self):
         # build the model, Y = computed output
         Y = tf.nn.softmax(tf.matmul(self.XX, self.W) + self.b)
         # define loss function
-        cross_entropy = -tf.reduce_mean(self.Y_ * tf.log(Y)) * 1000.0
+        self.loss_function = -tf.reduce_mean(self.Y_ * tf.log(Y)) * 1000.0
+        # define training step
+        self.train_step = tf.train.GradientDescentOptimizer(0.005).minimize(self.loss_function)
+        # define accuracy functions
+        self.correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(self.Y_, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
         # initialize model
-        sess = tf.Session()
-        sess.run(tf.global_variables_initializer())
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
         super().start()
 
     def process_signals(self, signals, input_id=None):
         for signal in signals:
-            pass
+            if input_id == 'train':
+                self._training(signal)
         self.notify_signals(signals)
+
+    def _training(self, signal):
+        batch_X, batch_Y = signal.batch
+        self.sess.run(self.train_step,
+                      feed_dict={self.X: batch_X, self.Y_: batch_Y})
--- tests/test_neural_network_block.py
+++ tests/test_neural_network_block.py
@@ -2,18 +2,21 @@ from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
 from ..neural_network_block import NeuralNetwork
+from unittest.mock import patch, MagicMock, ANY
 
 
 class TestNeuralNetworkBlock(NIOBlockTestCase):
 
-    def test_process_signals(self):
-        """Signals pass through block unmodified."""
+    @patch('tensorflow.Session')
+    def test_process_signals(self, mock_tf):
+        """numbers get computered like a motherfucker"""
+        input_signal = {'batch': (MagicMock(), MagicMock())}
         blk = NeuralNetwork()
         self.configure_block(blk, {})
         blk.start()
-        blk.process_signals([Signal({"hello": "n.io"})])
+        blk.process_signals([Signal(input_signal)], input_id='train')
         blk.stop()
         self.assert_num_signals_notified(1)
         self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "n.io"})
+            {'loss': ANY, 'accuracy': ANY, 'iteration': 1},
+            self.last_notified[DEFAULT_TERMINAL][0].to_dict())
