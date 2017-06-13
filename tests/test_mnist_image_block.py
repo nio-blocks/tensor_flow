e35dde456c9328f5ecd1a934ac8b9b6afb5d3b5b
Tom Young <tyoung@n.io>
2017-06-12 22:30:16 -0600

    rename mock object

--- tests/test_mnist_image_block.py
+++ tests/test_mnist_image_block.py
@@ -8,7 +8,7 @@ from ..mnist_image_block import MNISTImageLoader
 class TestMNISTImageLoader(NIOBlockTestCase):
 
     @patch('tensorflow.examples.tutorials.mnist.input_data.read_data_sets')
-    def test_process_signals(self, mock_read):
+    def test_process_signals(self, mock_dataset):
         """For each input signal call next_batch(batch_size) on the 
         corresponding attribute of returned DataSet object.
         """
@@ -18,24 +18,26 @@ class TestMNISTImageLoader(NIOBlockTestCase):
         blk.process_signals([Signal({'foo': 10})], input_id='train')
         blk.process_signals([Signal({'foo': 1})], input_id='test')
         blk.stop()
+        # todo: assert mock_dataset arga
+        print(mock_dataset.call_args_list)
         self.assert_num_signals_notified(2)
         self.assertDictEqual({'batch': ANY},
                              self.last_notified[DEFAULT_TERMINAL][0].to_dict())
-        mock_read.return_value.train.next_batch.assert_called_once_with(
+        mock_dataset.return_value.train.next_batch.assert_called_once_with(
             batch_size=10,
             shuffle=True)
-        mock_read.return_value.test.next_batch.assert_called_once_with(
+        mock_dataset.return_value.test.next_batch.assert_called_once_with(
             batch_size=1,
             shuffle=True)
 
     @patch('tensorflow.examples.tutorials.mnist.input_data.read_data_sets')
-    def test_shuffle_images(self, mock_read):
+    def test_shuffle_images(self, mock_dataset):
         """Shuffle can be disabled for repeatable output."""
         blk = MNISTImageLoader()
         self.configure_block(blk, {'shuffle': False})
         blk.start()
         blk.process_signals([Signal()], input_id='train')
         blk.stop()
-        mock_read.return_value.train.next_batch.assert_called_once_with(
+        mock_dataset.return_value.train.next_batch.assert_called_once_with(
             batch_size=100,
             shuffle=False)
